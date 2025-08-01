739a12c34e3d3868d61addf9099480d9863437ad
J <123409792+Jeyphr@users.noreply.github.com>
2025-08-01 08:44:54 -0700

    Updated Playermovement

--- Assets/Prefabs/Player/PlayerMovement.cs
+++ Assets/Prefabs/Player/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     //VARS
     [Header("Player Statistics")]
     public bool playerFrozen;
+    public bool updatePlayer;
     public float movementSpeed = 10f;
     public float walkMult = 0.5f;
     public float jumpPower = 5f;
@@ -15,16 +16,16 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Camera Statistics")]
     public bool cameraFrozen;
+    public bool freeCam;
     public float hSens = 1.5f;
     public float vSens = 1f;
     public float fov;
 
 
     [Header("Object References")]
-    public Camera pCam;
-    public PlayerInput pInput;
-    public CharacterController pController;
-    public PlayerInputHandler iHandler;
+    private Camera playerCamera;
+    private CharacterController playerController;
+    private PlayerInputHandler iHandler;
 
     //hiddenvars
     private Vector3 currentMovement;
@@ -40,13 +41,13 @@ public class PlayerMovement : MonoBehaviour
 
     void Awake()
     {
-        pController = GetComponent<CharacterController>();
-        pCam = Camera.main;
+        playerController = GetComponent<CharacterController>();
+        playerCamera = Camera.main;
         iHandler = PlayerInputHandler.Instance;
     }
     void Update()
     {
-        if (playerFrozen) { return; }
+        if (!updatePlayer) { return; }
         handleMovement();
         handleRotation();
         handleJumping();
@@ -55,6 +56,8 @@ public class PlayerMovement : MonoBehaviour
     #region Schmoovement
     private void handleMovement()
     {
+        if (playerFrozen) { return; }
+
         float speed = movementSpeed * (iHandler.IsWalking ? walkMult : 1f);
         Vector3 inputDirection = new Vector3(iHandler.MoveInput.x, 0f, iHandler.MoveInput.y);
         Vector3 worldDirection = transform.TransformDirection(inputDirection);
@@ -62,21 +65,28 @@ public class PlayerMovement : MonoBehaviour
 
         currentMovement.x = worldDirection.x * speed;
         currentMovement.z = worldDirection.z * speed;
-        pController.Move(currentMovement * Time.deltaTime);
+        playerController.Move(currentMovement * Time.deltaTime);
     }
     private void handleRotation()
     {
-        mouseX = iHandler.LookInput.x * hSens;
-        transform.Rotate(0, mouseX, 0);
+        if(cameraFrozen){ return; }
 
-        mouseY -= iHandler.LookInput.y * vSens;
-        mouseY = Mathf.Clamp(mouseY, -upDownRange, upDownRange);
-        pCam.transform.localRotation = Quaternion.Euler(mouseY, 0, 0);
+        if (!freeCam) {
+            mouseX = iHandler.LookInput.x * hSens;
+            transform.Rotate(0, mouseX, 0);
+    
+            mouseY -= iHandler.LookInput.y * vSens;
+            mouseY = Mathf.Clamp(mouseY, -upDownRange, upDownRange);
+            playerCamera.transform.localRotation = Quaternion.Euler(mouseY, 0, 0);
+        }
+
     }
 
     private void handleJumping()
     {
-        if (pController.isGrounded)
+        if(playerFrozen){ return; }
+
+        if (playerController.isGrounded)
         {
             currentMovement.y = -0.5f;
 
@@ -89,13 +99,6 @@ public class PlayerMovement : MonoBehaviour
         {
             currentMovement.y -= gravity * Time.deltaTime;
         }
-    }
-    #endregion
-
-    #region Checks
-    private bool groundCheck()
-    {
-        return true;
     }
     #endregion
 }
